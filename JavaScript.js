c43940718d7761edf790cf3cb9f4347f2455fd3e
Humam Samra <Humamdaraya@gmail.com>
2023-01-13 20:57:47 +0300

    indexs

--- JavaScript.js
+++ JavaScript.js
@@ -2,7 +2,7 @@ $("form#LoginForm").submit(function (e){
     e.preventDefault();
     var username = document.getElementById('UserName').value;
     var password = document.getElementById('Password').value;
-    if(username.toLowerCase() == "admin" || password.toLowerCase() == "0000"){
+    if(username.toLowerCase() == "admin" && password.toLowerCase() == "0000"){
         Swal.fire(
             'Logged In',
             'Redirecting You...',
@@ -19,5 +19,11 @@ $("form#LoginForm").submit(function (e){
             title: 'Oops...',
             text: 'Wrong Username or Password',
           })
+    }
+})
+
+$(document).ready(function (){
+    if(sessionStorage.getItem("Logged") == '1'){
+        window.location.href = "Generator.html";
     }
 })
