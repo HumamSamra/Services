b2d462fbc8aa973146a21ad5681e2b5ce71777ee
Humam Samra <Humamdaraya@gmail.com>
2023-01-13 15:19:57 +0300

    Shadow

--- JavaScript.js
+++ JavaScript.js
@@ -10,7 +10,7 @@ $("form#LoginForm").submit(function (e){
           )
         sessionStorage.setItem("Logged", "1");
         setTimeout(() => {
-            window.location.href = "Lobby.html";
+            window.location.href = "Lobby";
         }, 500);
 
     }else{
