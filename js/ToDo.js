b3ffba0bd26c53e6603429f73e1b82d3ed9a957c
Humam Samra <Humamdaraya@gmail.com>
2023-01-15 12:22:27 +0300

    Inputs

--- js/ToDo.js
+++ js/ToDo.js
@@ -29,13 +29,19 @@ if (!localStorage.getItem("Logged")) {
             +'<div class="rounded-circle flex-shrink-0 mt-auto mb-auto" onclick="Remove(' + i.toString() + ')" style="cursor: pointer;">'
             +'<svg style="color: crimson;" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3-fill" viewBox="0 0 16 16">'
             +'<path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5Zm-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5ZM4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06Zm6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528ZM8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5Z"></path></svg>'
-            +'</div><div class="d-flex gap-2 w-100 justify-content-between"><div><h6 class="mb-0" id="term_' + i.toString() + '">' + val[0] + '</h6>'
+            +'</div><div class="d-flex gap-2 w-100 justify-content-between"><div class="w-100"><input class="mb-0 form-control" oninput="Update(' + i.toString() + ')" id="term_' + i.toString() + '" value="' + val[0] + '">'
                 + '</div><small class="opacity-50 text-nowrap mt-auto mb-auto">' + val[1] +'</small></div></h>');
         } 
     }
 
   }
   
+  function Update(i){
+    const date = new Date();
+    let input = document.getElementById("term_" + i).value;
+    var val = localStorage.setItem(i, input + "[/[" + date.toDateString());
+  }
+
   function Add(){
     var input = document.getElementById('floatingInput');
     for(let i = 1; i < 1000; i++){
@@ -69,7 +75,7 @@ if (!localStorage.getItem("Logged")) {
         return;
     }
     for(let i = 1; i < count + 1; i++){
-        var ele = document.getElementById('term_' + i).innerHTML.toString().toLowerCase();
+        var ele = document.getElementById('term_' + i).value.toString().toLowerCase();
         let parent = document.getElementById("item_" + i);
        if(ele.includes(search)){
             parent.classList.remove("d-none");
